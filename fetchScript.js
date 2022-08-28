79b92ebe0c3d77dc61368a9a32a01b9a242fefa4
AKSH DESAI <68705063+AKSHDESAI1@users.noreply.github.com>
2022-08-28 15:31:16 +0530

    Add files via upload

---
+++ fetchScript.js
@@ -0,0 +1,27 @@
+let btn = document.getElementById("btn");
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+let body1 = document.getElementById("body1");
+
+const makeRequest = () => {
+    // console.log("Button Clicked");
+    // btn.setAttribute('disabled', 'disabled');
+    first.classList.add('d-none');
+    second.classList.remove('d-none');
+
+    const promiseObj = fetch("data.txt");
+    console.log('promiseObj', promiseObj);
+    promiseObj.then((res) => {
+        console.log('meow');
+        console.log('res', res);
+        return res.text();
+    }).then((data) => {
+        // btn.removeAttribute('disabled');
+        first.classList.remove('d-none');
+        second.classList.add('d-none');    
+        console.log('data', data);
+        body1.innerHTML = data;
+    })
+}
+
+btn.addEventListener('click', makeRequest); 1
